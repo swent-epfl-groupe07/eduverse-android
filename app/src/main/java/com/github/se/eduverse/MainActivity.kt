5c35f280e99e22930e9958f2487a67bca3ed3a90
Ahmed kallala <ahmedkallala@s23-de-tchatchou.home>
2024-10-09 15:05:18 +0200

    Initialize the android project

---
+++ app/src/main/java/com/github/se/eduverse/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.github.se.eduverse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.github.se.bootcamp.ui.theme.EduverseTheme
+
+class MainActivity : ComponentActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      EduverseTheme { Surface(modifier = Modifier.fillMaxSize()) {} }
+    }
+  }
+}
+
