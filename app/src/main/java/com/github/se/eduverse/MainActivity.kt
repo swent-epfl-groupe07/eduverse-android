f7022ffa34ec764d527451d2c2fcf76725ebc26e
Ahmed kallala <ahmedkallala@s23-de-tchatchou.home>
2024-10-10 16:21:04 +0200

    Add login logic

--- app/src/main/java/com/github/se/eduverse/MainActivity.kt
+++ app/src/main/java/com/github/se/eduverse/MainActivity.kt
@@ -5,13 +5,37 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
+import com.github.se.eduverse.ui.authentication.LoginScreen
+import com.github.se.eduverse.ui.camera.CameraScreen
+import com.github.se.eduverse.ui.dashboard.DashboardScreen
+import com.github.se.eduverse.ui.navigation.NavigationActions
+import com.github.se.eduverse.ui.navigation.Route
+import com.github.se.eduverse.ui.navigation.Screen
+import com.github.se.eduverse.ui.others.OthersScreen
 import com.github.se.eduverse.ui.theme.EduverseTheme
+import com.github.se.eduverse.ui.videos.VideosScreen
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
 
+  private lateinit var auth: FirebaseAuth
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent { EduverseTheme { Surface(modifier = Modifier.fillMaxSize()) {} } }
+
+    // Initialize Firebase Auth
+    auth = FirebaseAuth.getInstance()
+    auth.currentUser?.let {
+      // Sign out the user if they are already signed in
+      // This is useful for testing purposes
+      auth.signOut()
+    }
+    setContent { EduverseTheme { Surface(modifier = Modifier.fillMaxSize()) { } } }
   }
 }
