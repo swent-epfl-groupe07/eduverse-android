f7d78ea9a446ac6087401f13a4c62eb6c2beb538
Mathieu COLLET <mathieu.collet@epfl.ch>
2024-10-10 10:48:27 +0200

    Create the top and bottom bars based on the bootcamp model

--- app/src/main/java/com/github/se/eduverse/ui/folder/folderScreen.kt
+++ app/src/main/java/com/github/se/eduverse/ui/folder/folderScreen.kt
@@ -1,12 +1,66 @@
 package com.github.se.project.ui
 
 import android.annotation.SuppressLint
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextOverflow
+import com.github.se.eduverse.ui.navigation.BottomNavigationMenu
+import com.github.se.eduverse.ui.navigation.LIST_TOP_LEVEL_DESTINATION
+import com.github.se.eduverse.ui.navigation.NavigationActions
+import com.github.se.project.model.folder.FolderViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun folderScreen() {
-  Scaffold { Text("Folder Screen") }
+fun FolderScreen(navigationActions: NavigationActions, folderViewModel: FolderViewModel) {
+  if (folderViewModel.activeFolder == null) throw IllegalArgumentException(
+    "There is no active folder, select one before going to FolderScreen")
+  val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
+  Scaffold(
+    topBar = {
+      MediumTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+          containerColor = MaterialTheme.colorScheme.primaryContainer,
+          titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+          Text(
+            text = folderViewModel.activeFolder!!.name,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis)
+        },
+        navigationIcon = {
+          IconButton(
+            onClick = { navigationActions.goBack() }) {
+            Icon(
+              imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+              contentDescription = "Back Arrow")
+          }
+        },
+        scrollBehavior = scrollBehavior
+      )
+    },
+    bottomBar = {
+      BottomNavigationMenu(
+        { navigationActions.navigateTo(it) },
+        LIST_TOP_LEVEL_DESTINATION,
+        "" //No item is selected, as it is not one of the screens on the bottom bar
+      )
+    }
+  ) {
+    Text("Folder Screen")
+  }
 }
