ef32ba70463ffb936c22c837b03cca23fede7eda
Ahmed kallala <ahmedkallala@s23-de-tchatchou.home>
2024-10-10 17:20:08 +0200

    feature(nav): Add navigation logic to MainActivity

--- app/src/main/java/com/github/se/eduverse/MainActivity.kt
+++ app/src/main/java/com/github/se/eduverse/MainActivity.kt
@@ -20,22 +20,54 @@ import com.github.se.eduverse.ui.navigation.Screen
 import com.github.se.eduverse.ui.others.OthersScreen
 import com.github.se.eduverse.ui.theme.EduverseTheme
 import com.github.se.eduverse.ui.videos.VideosScreen
-import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
 
-  private lateinit var auth: FirebaseAuth
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    setContent { EduverseTheme { Surface(modifier = Modifier.fillMaxSize()) { EduverseApp() } } }
+  }
+}
 
-    // Initialize Firebase Auth
-    auth = FirebaseAuth.getInstance()
-    auth.currentUser?.let {
-      // Sign out the user if they are already signed in
-      // This is useful for testing purposes
-      auth.signOut()
+@Composable
+fun EduverseApp() {
+  val navController = rememberNavController()
+  val navigationActions = NavigationActions(navController)
+
+  NavHost(navController = navController, startDestination = Route.AUTH) {
+    navigation(
+        startDestination = Screen.AUTH,
+        route = Route.AUTH,
+    ) {
+      composable(Screen.AUTH) { LoginScreen(navigationActions) }
     }
-    setContent { EduverseTheme { Surface(modifier = Modifier.fillMaxSize()) { } } }
+
+    navigation(
+        startDestination = Screen.DASHBOARD,
+        route = Route.DASHBOARD,
+    ) {
+      composable(Screen.DASHBOARD) { DashboardScreen(navigationActions) }
+    }
+
+    navigation(
+        startDestination = Screen.VIDEOS,
+        route = Route.VIDEOS,
+    ) {
+      composable(Screen.VIDEOS) { VideosScreen(navigationActions) }
+    }
+
+    navigation(
+        startDestination = Screen.CAMERA,
+        route = Route.CAMERA,
+    ) {
+      composable(Screen.CAMERA) { CameraScreen(navigationActions) }
+    }
+
+    navigation(
+        startDestination = Screen.OTHERS,
+        route = Route.OTHERS,
+    ) {
+      composable(Screen.OTHERS) { OthersScreen(navigationActions) }
+    }
   }
 }
