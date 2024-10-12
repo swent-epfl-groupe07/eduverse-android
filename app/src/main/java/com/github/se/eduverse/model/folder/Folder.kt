552193bab1d5a3caa66991a14d0ac3b3f2902ce1
Mathieu COLLET <mathieu.collet@epfl.ch>
2024-10-12 17:41:38 +0200

    Implement Folder class

--- app/src/main/java/com/github/se/eduverse/model/folder/Folder.kt
+++ app/src/main/java/com/github/se/eduverse/model/folder/Folder.kt
@@ -1,4 +1,18 @@
 package com.github.se.eduverse.model.folder
 
-class Folder {
+data class Folder(
+    val files: MutableList<MyFile>,
+    val name: String,
+    val id: String,
+    val timeTable: TimeTable
+)
+
+enum class FilterTypes {
+    NAME,
+    CREATION_UP,
+    CREATION_DOWN,
+    ACCESS_RECENT,
+    ACCESS_OLD,
+    ACCESS_MOST,
+    ACCESS_LEAST
 }
