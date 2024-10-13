97ecfc7923125f394e466f5efc472ae72ffbdbad
Abdellah JANATI IDRISSI <abdellah@MacBook-Pro-de-Abdellah.local>
2024-10-13 17:55:16 +0200

    add login test for user authentication

---
+++ app/src/androidTest/java/com/github/se/eduverse/ui/authentication/LoginTest.kt
@@ -0,0 +1,51 @@
+package com.github.se.bootcamp.ui.authentication
+
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.se.eduverse.MainActivity
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LoginTest : TestCase() {
+  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+  @Before
+  fun setUp() {
+    Intents.init()
+  }
+
+  @After
+  fun tearDown() {
+    Intents.release()
+  }
+
+  @Test
+  fun titleAndButtonAreCorrectlyDisplayed() {
+    composeTestRule.onNodeWithTag("loginText").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("loginText").assertTextEquals("Login or Sign-Up via Google")
+
+    composeTestRule.onNodeWithTag("loginButton").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("loginButton").assertHasClickAction()
+  }
+
+  @Test
+  fun googleSignInReturnsValidActivityResult() {
+    composeTestRule.onNodeWithTag("loginButton").performClick()
+    composeTestRule.waitForIdle()
+
+    intended(toPackage("com.google.android.gms"))
+  }
+}
