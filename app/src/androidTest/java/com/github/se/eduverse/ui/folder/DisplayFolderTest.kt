d84fd677f8a1a6203ee37e49ad127ea74c93e0af
Mathieu COLLET <mathieu.collet@epfl.ch>
2024-10-14 17:12:17 +0200

    Improve coverage

--- app/src/androidTest/java/com/github/se/eduverse/ui/folder/DisplayFolderTest.kt
+++ app/src/androidTest/java/com/github/se/eduverse/ui/folder/DisplayFolderTest.kt
@@ -4,17 +4,21 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.github.se.eduverse.model.folder.Folder
 import com.github.se.eduverse.model.folder.FolderRepository
 import com.github.se.eduverse.model.folder.FolderViewModel
 import com.github.se.eduverse.model.folder.MyFile
+import com.github.se.eduverse.ui.navigation.LIST_TOP_LEVEL_DESTINATION
 import com.github.se.eduverse.ui.navigation.NavigationActions
+import com.github.se.eduverse.ui.navigation.TopLevelDestination
 import com.google.firebase.auth.FirebaseUser
 import java.util.Calendar
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mockito.doAnswer
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
@@ -27,10 +31,8 @@ class DisplayFolderTest {
   private lateinit var folderViewModel: FolderViewModel
 
   val file1 = MyFile("", "name 1", Calendar.getInstance(), Calendar.getInstance(), 0)
-  val file2 =
-      MyFile("", "name 2", java.util.Calendar.getInstance(), java.util.Calendar.getInstance(), 0)
-  val file3 =
-      MyFile("", "name 3", java.util.Calendar.getInstance(), java.util.Calendar.getInstance(), 0)
+  val file2 = MyFile("", "name 2", Calendar.getInstance(), Calendar.getInstance(), 0)
+  val file3 = MyFile("", "name 3", Calendar.getInstance(), Calendar.getInstance(), 0)
 
   val folder =
       Folder(
@@ -70,10 +72,54 @@ class DisplayFolderTest {
 
   @Test
   fun displayComponents() {
+    composeTestRule.onNodeWithTag("goBack").assertIsDisplayed()
     composeTestRule.onNodeWithTag("topAppBar").assertIsDisplayed()
     composeTestRule.onNodeWithTag("createFile").assertIsDisplayed()
     composeTestRule.onNodeWithTag("textFiles").assertIsDisplayed()
     composeTestRule.onNodeWithTag("sortingButton").assertIsDisplayed()
+  }
+
+  @Test
+  fun goBackWorks() {
+    var test = false
+    `when`(navigationActions.goBack()).then {
+      test = true
+      null
+    }
+
+    composeTestRule.onNodeWithTag("goBack").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("goBack").performClick()
+    assert(test)
+  }
+
+  @Test
+  fun bottomBarWorks() {
+    var test: Boolean
+    `when`(navigationActions.navigateTo(any<TopLevelDestination>())).then {
+      test = true
+      null
+    }
+    LIST_TOP_LEVEL_DESTINATION.forEach {
+      test = false
+
+      composeTestRule.onNodeWithText(it.textId).assertIsDisplayed()
+      composeTestRule.onNodeWithText(it.textId).performClick()
+
+      assert(test)
+    }
+  }
+
+  @Test
+  fun addFileButtonWorks() {
+    var test = false
+    `when`(navigationActions.navigateTo(anyString())).then {
+      test = true
+      null
+    }
+    composeTestRule.onNodeWithTag("createFile").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("createFile").performClick()
+
+    assert(test)
   }
 
   @Test
@@ -92,5 +138,35 @@ class DisplayFolderTest {
 
     composeTestRule.onNodeWithTag("sortMode1").performClick()
     composeTestRule.onNodeWithTag("sortMode1").assertIsNotDisplayed()
+
+    composeTestRule.onNodeWithTag("sortingButton").performClick()
+    composeTestRule.onNodeWithTag("sortMode2").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("sortMode2").performClick()
+    composeTestRule.onNodeWithTag("sortMode2").assertIsNotDisplayed()
+
+    composeTestRule.onNodeWithTag("sortingButton").performClick()
+    composeTestRule.onNodeWithTag("sortMode3").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("sortMode3").performClick()
+    composeTestRule.onNodeWithTag("sortMode3").assertIsNotDisplayed()
+
+    composeTestRule.onNodeWithTag("sortingButton").performClick()
+    composeTestRule.onNodeWithTag("sortMode4").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("sortMode4").performClick()
+    composeTestRule.onNodeWithTag("sortMode4").assertIsNotDisplayed()
+
+    composeTestRule.onNodeWithTag("sortingButton").performClick()
+    composeTestRule.onNodeWithTag("sortMode5").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("sortMode5").performClick()
+    composeTestRule.onNodeWithTag("sortMode5").assertIsNotDisplayed()
+
+    composeTestRule.onNodeWithTag("sortingButton").performClick()
+    composeTestRule.onNodeWithTag("sortMode6").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("sortMode6").performClick()
+    composeTestRule.onNodeWithTag("sortMode6").assertIsNotDisplayed()
+
+    composeTestRule.onNodeWithTag("sortingButton").performClick()
+    composeTestRule.onNodeWithTag("sortMode7").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("sortMode7").performClick()
+    composeTestRule.onNodeWithTag("sortMode7").assertIsNotDisplayed()
   }
 }
