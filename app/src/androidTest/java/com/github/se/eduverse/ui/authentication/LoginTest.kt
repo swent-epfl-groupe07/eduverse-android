17f76abb5f934924ea5d8015b9ee9a867d39dc48
Abdellah JANATI IDRISSI <abdellah@MacBook-Pro-de-Abdellah.local>
2024-10-15 16:15:37 +0100

    fixing bugs in LoginTests

--- app/src/androidTest/java/com/github/se/eduverse/ui/authentication/LoginTest.kt
+++ app/src/androidTest/java/com/github/se/eduverse/ui/authentication/LoginTest.kt
@@ -3,28 +3,42 @@ package com.github.se.eduverse.ui.authentication
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.navigation.NavHostController
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.se.eduverse.MainActivity
-import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import com.github.se.eduverse.ui.authentification.SignInScreen
+import com.github.se.eduverse.ui.navigation.NavigationActions
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
+
 
 @RunWith(AndroidJUnit4::class)
-class LoginTest : TestCase() {
-  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+class LoginTest {
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  private lateinit var navController: NavHostController
+  private lateinit var navigationActions: NavigationActions
 
   @Before
   fun setUp() {
     Intents.init()
+
+    navController = Mockito.mock(NavHostController::class.java)
+    navigationActions = NavigationActions(navController)
+
+    composeTestRule.setContent {
+      SignInScreen(navigationActions = navigationActions)
+    }
   }
 
   @After
