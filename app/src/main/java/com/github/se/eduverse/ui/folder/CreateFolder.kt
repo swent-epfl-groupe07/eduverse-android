3b9396e440d523ba3c4a523b94d14a3bdff556d7
Mathieu COLLET <mathieu.collet@epfl.ch>
2024-10-15 11:14:17 +0200

    Create the screen to create a folder

---
+++ app/src/main/java/com/github/se/eduverse/ui/folder/CreateFolder.kt
@@ -0,0 +1,175 @@
+package com.github.se.eduverse.ui.folder
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.se.eduverse.model.folder.Folder
+import com.github.se.eduverse.model.folder.MyFile
+import com.github.se.eduverse.ui.navigation.BottomNavigationMenu
+import com.github.se.eduverse.ui.navigation.LIST_TOP_LEVEL_DESTINATION
+import com.github.se.eduverse.ui.navigation.NavigationActions
+import com.github.se.eduverse.ui.navigation.Screen
+import com.github.se.eduverse.ui.navigation.TopLevelDestinations
+import com.github.se.eduverse.viewmodel.FolderViewModel
+import java.util.Calendar
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddFolderScreen(
+    navigationActions: NavigationActions, folderViewModel: FolderViewModel
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+    var name by rememberSaveable { mutableStateOf("") }
+    var files by rememberSaveable { mutableStateOf(emptyList<MyFile>()) }
+    val folder = Folder(
+        ownerID = folderViewModel.currentUser!!.uid,
+        files = files.toMutableList(),
+        name = name,
+        id = folderViewModel.getNewFolderUid())
+
+    Scaffold(
+        modifier = Modifier.testTag("scaffold"),
+        topBar = {
+            MediumTopAppBar(
+                modifier = Modifier.testTag("topAppBar"),
+                colors =
+                TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        text = "Create Course",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.testTag("topBarText"))
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navigationActions.goBack() }, modifier = Modifier.testTag("goBack")) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back Arrow")
+                    }
+                },
+                scrollBehavior = scrollBehavior)
+        },
+        bottomBar = {
+            BottomNavigationMenu(
+                { navigationActions.navigateTo(it) },
+                LIST_TOP_LEVEL_DESTINATION,
+                "") // No item is selected, as it is not one of the screens on the bottom bar
+        }) { padding ->
+        Column(modifier = Modifier.padding(padding)) {
+            // Give a name to the course
+            Text(
+                "Course Name",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                modifier = Modifier.testTag("textFiles"))
+            OutlinedTextField(
+                value = name,
+                modifier =
+                Modifier.fillMaxWidth()
+                    .padding(horizontal = 15.dp, vertical = 10.dp)
+                    .testTag("inputTodoTitle"),
+                onValueChange = { name = it },
+                placeholder = { Text("Name of the course") })
+
+
+            // Add file to the course
+            Text(
+                "Files",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                modifier = Modifier.testTag("textFiles"))
+            files.forEach {
+                Button(
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp).testTag(it.name)) {
+                    Text(it.name, modifier = Modifier.fillMaxWidth())
+                }
+            }
+            Button(
+                onClick = {
+                    navigationActions.navigateTo(Screen.CREATE_FILE)
+                    files += MyFile(
+                        id = folderViewModel.getNewFileUid(folder),
+                        fileId = "fileId",
+                        name = "fileName",
+                        creationTime = Calendar.getInstance(),
+                        lastAccess = Calendar.getInstance(),
+                        numberAccess = 0
+                    )
+                },
+                modifier =
+                Modifier.fillMaxWidth()
+                    .padding(horizontal = 15.dp, vertical = 10.dp)
+                    .testTag("addFile")) {
+                Text("Add file")
+            }
+
+
+            // Create the folder
+            Button(
+                onClick = {
+                    folderViewModel.addFolder(folder)
+                    files = emptyList()
+                    name = ""
+                    navigationActions.navigateTo(TopLevelDestinations.DASHBOARD)
+                },
+                enabled = name.isNotEmpty(),
+                modifier =
+                Modifier.fillMaxWidth()
+                    .padding(horizontal = 15.dp, vertical = 10.dp)
+                    .testTag("folderSave")) {
+                Text("Save")
+            }
+
+
+            // Cancel the folder creation
+            Button(
+                onClick = {
+                    // Because we have created the folder in db when calling getNewFolderUid :
+                    folderViewModel.deleteFolder(folder)
+
+                    // Because files and name are saveable :
+                    files = emptyList()
+                    name = ""
+
+                    navigationActions.goBack()
+                },
+                modifier =
+                Modifier.fillMaxWidth()
+                    .padding(horizontal = 15.dp, vertical = 10.dp)
+                    .testTag("folderCancel")) {
+                Text("Cancel")
+            }
+        }
+    }
+}
