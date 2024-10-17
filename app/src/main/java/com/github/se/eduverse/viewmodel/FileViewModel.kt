cbd6aee03d0a816e3ed7d8f5f625c7ac65d899ca
Mathieu COLLET <mathieu.collet@epfl.ch>
2024-10-17 22:29:03 +0200

    Fix forgotten test element

--- app/src/main/java/com/github/se/eduverse/viewmodel/FileViewModel.kt
+++ app/src/main/java/com/github/se/eduverse/viewmodel/FileViewModel.kt
@@ -1,8 +1,10 @@
 package com.github.se.eduverse.viewmodel
 
 import android.net.Uri
+import android.util.Log
 import com.github.se.eduverse.model.folder.MyFile
 import com.github.se.eduverse.repository.FileRepository
+import java.util.Calendar
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -19,8 +21,8 @@ class FileViewModel(val fileRepository: FileRepository) {
   val validNewFile: StateFlow<Boolean> = _validNewFile
 
   fun createFile(uri: Uri) {
-    _validNewFile.value = true
-    /*_newFile.value = null // In case the creation fails, users will see it
+    _validNewFile.value = false
+    _newFile.value = null // In case the creation fails, users will see it
     val uid = fileRepository.getNewUid()
     fileRepository.saveFile(
         uri,
@@ -39,7 +41,7 @@ class FileViewModel(val fileRepository: FileRepository) {
                   lastAccess = Calendar.getInstance(),
                   numberAccess = 0)
         },
-        { Log.e("File Upload", "Uploading of file ${uri.lastPathSegment} failed: $it") })*/
+        { Log.e("File Upload", "Uploading of file ${uri.lastPathSegment} failed: $it") })
   }
 
   fun setName(name: String) {
