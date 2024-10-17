f79ea9f905dd215f1db7d1dbf4389ea1350a949f
Mathieu COLLET <mathieu.collet@epfl.ch>
2024-10-17 23:17:20 +0200

    Test and document the file repository

--- app/src/main/java/com/github/se/eduverse/repository/FileRepositoryImpl.kt
+++ app/src/main/java/com/github/se/eduverse/repository/FileRepositoryImpl.kt
@@ -9,10 +9,23 @@ class FileRepositoryImpl(private val db: FirebaseFirestore, private val storage:
     FileRepository {
   private val collectionPath = "files"
 
+    /**
+     * Create a new file document in the firebase firestore
+     *
+     * @return the id of the document
+     */
   override fun getNewUid(): String {
     return db.collection(collectionPath).document().id
   }
 
+    /**
+     * Save a file in the firebase storage, and save the path to it in a file on firestore
+     *
+     * @param file the URI of the file to add
+     * @param fileId the id of the firestore document in which the path should be stored
+     * @param onSuccess to execute if the task is done successfully
+     * @param onFailure error management method
+     */
   override fun saveFile(
       file: Uri,
       fileId: String,
@@ -32,6 +45,9 @@ class FileRepositoryImpl(private val db: FirebaseFirestore, private val storage:
         .addOnFailureListener(onFailure)
   }
 
+    /**
+     * Does nothing for now
+     */
   override fun modifiyFile(
       file: Uri,
       fileId: String,
@@ -41,6 +57,9 @@ class FileRepositoryImpl(private val db: FirebaseFirestore, private val storage:
     TODO("Not yet implemented")
   }
 
+    /**
+     * Does nothing for now
+     */
   override fun deleteFile(
       file: Uri,
       fileId: String,
@@ -50,6 +69,9 @@ class FileRepositoryImpl(private val db: FirebaseFirestore, private val storage:
     TODO("Not yet implemented")
   }
 
+    /**
+     * Does nothing for now
+     */
   override fun accessFile(
       file: Uri,
       fileId: String,
@@ -59,7 +81,14 @@ class FileRepositoryImpl(private val db: FirebaseFirestore, private val storage:
     TODO("Not yet implemented")
   }
 
-  private fun savePDFUrlToFirestore(path: String, fileId: String, onSuccess: () -> Unit) {
+    /**
+     * Save the path to a file on firebase. Isn't private for testing purpose, but shouldn't be called.
+     *
+     * @param path the path to save
+     * @param fileId the id of the document in which the path should be stored
+     * @param onSuccess to execute in case of success
+     */
+  fun savePDFUrlToFirestore(path: String, fileId: String, onSuccess: () -> Unit) {
     val pdfData = hashMapOf("url" to path)
     db.collection(collectionPath)
         .document(fileId)
