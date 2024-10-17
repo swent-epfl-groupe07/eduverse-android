1172152e38a8cb093240f7520366429973bdd99a
AKallala <ahmed.kallala@hotmail.fr>
2024-10-18 00:35:50 +0200

    Update Others.kt: add pdf converter button on click action

--- app/src/main/java/com/github/se/eduverse/ui/others/Others.kt
+++ app/src/main/java/com/github/se/eduverse/ui/others/Others.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.unit.sp
 import com.github.se.eduverse.ui.navigation.BottomNavigationMenu
 import com.github.se.eduverse.ui.navigation.LIST_TOP_LEVEL_DESTINATION
 import com.github.se.eduverse.ui.navigation.NavigationActions
+import com.github.se.eduverse.ui.navigation.Screen
 
 @Composable
 fun OthersScreen(navigationActions: NavigationActions) {
@@ -71,22 +72,22 @@ fun OthersScreen(navigationActions: NavigationActions) {
 
               Spacer(modifier = Modifier.height(16.dp))
 
-              // Field #4 button
+              // Pdf Converter
               Button(
-                  onClick = { /* Placeholder for Field #4 */},
+                  onClick = { navigationActions.navigateTo(Screen.PDF_CONVERTER)},
                   modifier =
                       Modifier.fillMaxWidth()
                           .height(50.dp)
-                          .testTag("field4Button") // Test tag for Field #4 button
+                          .testTag("pdfConverterButton") // Test tag for Field #4 button
                   ) {
-                    Text(text = "Field #4", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                    Text(text = "Pdf Converter", fontSize = 20.sp, fontWeight = FontWeight.Bold)
                   }
 
               Spacer(modifier = Modifier.height(16.dp))
 
               // Field #5 button
               Button(
-                  onClick = { /* Placeholder for Field #5 */},
+                  onClick = {  },
                   modifier =
                       Modifier.fillMaxWidth()
                           .height(50.dp)
