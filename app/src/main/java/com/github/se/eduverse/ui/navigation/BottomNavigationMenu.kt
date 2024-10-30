d2eff7c5319806a94f78fb6057a4efb131402136
AKallala <ahmed.kallala@hotmail.fr>
2024-10-30 20:33:13 +0100

    Update navigation bar with new destinations and design

--- app/src/main/java/com/github/se/eduverse/ui/navigation/BottomNavigationMenu.kt
+++ app/src/main/java/com/github/se/eduverse/ui/navigation/BottomNavigationMenu.kt
@@ -8,14 +8,19 @@ package com.github.se.eduverse.ui.navigation
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 
@@ -25,18 +30,27 @@ fun BottomNavigationMenu(
     tabList: List<TopLevelDestination>,
     selectedItem: String
 ) {
-  BottomNavigation(
+  val context = LocalContext.current
+  NavigationBar(
       modifier = Modifier.fillMaxWidth().height(60.dp).testTag("bottomNavigationMenu"),
-      backgroundColor = MaterialTheme.colorScheme.surface,
-      content = {
+      containerColor = Color(0xFFD4DEE8) // Background color for the NavigationBar
+      ) {
         tabList.forEach { tab ->
-          BottomNavigationItem(
+          NavigationBarItem(
               icon = { Icon(tab.icon, contentDescription = null) },
               label = { Text(tab.textId) },
-              selected = tab.route == selectedItem,
+              colors =
+                  NavigationBarItemColors(
+                      Color.Black,
+                      Color.Black,
+                      Color(0xFF37CED5),
+                      Color.Black,
+                      Color.Black,
+                      Color.Gray,
+                      Color.Gray),
+              selected = selectedItem == tab.route,
               onClick = { onTabSelect(tab) },
               modifier = Modifier.clip(RoundedCornerShape(50.dp)).testTag(tab.textId))
         }
-      },
-  )
+      }
 }
