ecea073558bd6287ef35135d11ea9a3bbbfc3ebe
Mathieu COLLET <mathieu.collet@epfl.ch>
2024-10-30 22:39:58 +0100

    Make MyFile parcelable

--- app/src/main/java/com/github/se/eduverse/model/MyFile.kt
+++ app/src/main/java/com/github/se/eduverse/model/MyFile.kt
@@ -1,5 +1,7 @@
 package com.github.se.eduverse.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import java.util.Calendar
 
 data class MyFile(
@@ -9,4 +11,39 @@ data class MyFile(
     val creationTime: Calendar,
     var lastAccess: Calendar,
     var numberAccess: Int
-)
+) : Parcelable {
+
+  // Parcelable implementation
+  constructor(
+      parcel: Parcel
+  ) : this(
+      id = parcel.readString() ?: "",
+      fileId = parcel.readString() ?: "",
+      name = parcel.readString() ?: "",
+      creationTime = Calendar.getInstance().apply { timeInMillis = parcel.readLong() },
+      lastAccess = Calendar.getInstance().apply { timeInMillis = parcel.readLong() },
+      numberAccess = parcel.readInt())
+
+  override fun writeToParcel(parcel: Parcel, flags: Int) {
+    parcel.writeString(id)
+    parcel.writeString(fileId)
+    parcel.writeString(name)
+    parcel.writeLong(creationTime.timeInMillis)
+    parcel.writeLong(lastAccess.timeInMillis)
+    parcel.writeInt(numberAccess)
+  }
+
+  override fun describeContents(): Int {
+    return 0
+  }
+
+  companion object CREATOR : Parcelable.Creator<MyFile> {
+    override fun createFromParcel(parcel: Parcel): MyFile {
+      return MyFile(parcel)
+    }
+
+    override fun newArray(size: Int): Array<MyFile?> {
+      return arrayOfNulls(size)
+    }
+  }
+}
