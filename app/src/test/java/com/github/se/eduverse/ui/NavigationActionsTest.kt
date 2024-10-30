bb053f755ccba7480624028d8e19244f41028471
AKallala <ahmed.kallala@hotmail.fr>
2024-10-30 21:20:49 +0100

    Update NavigationActionsTest.kt after changes

--- app/src/test/java/com/github/se/eduverse/ui/NavigationActionsTest.kt
+++ app/src/test/java/com/github/se/eduverse/ui/NavigationActionsTest.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavOptionsBuilder
 import com.github.se.eduverse.ui.navigation.NavigationActions
 import com.github.se.eduverse.ui.navigation.Route
 import com.github.se.eduverse.ui.navigation.Screen
+import com.github.se.eduverse.ui.navigation.TopLevelDestination
 import com.github.se.eduverse.ui.navigation.TopLevelDestinations
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
@@ -38,17 +39,38 @@ class NavigationActionsTest {
     navigationActions.navigateTo(TopLevelDestinations.CAMERA)
     verify(navHostController).navigate(eq(Route.CAMERA), any<NavOptionsBuilder.() -> Unit>())
 
-    navigationActions.navigateTo(TopLevelDestinations.OTHERS)
-    verify(navHostController).navigate(eq(Route.OTHERS), any<NavOptionsBuilder.() -> Unit>())
+    navigationActions.navigateTo(TopLevelDestinations.PROFILE)
+    verify(navHostController).navigate(eq(Route.PROFILE), any<NavOptionsBuilder.() -> Unit>())
 
     navigationActions.navigateTo(TopLevelDestinations.VIDEOS)
     verify(navHostController).navigate(eq(Route.VIDEOS), any<NavOptionsBuilder.() -> Unit>())
 
+    navigationActions.navigateTo(TopLevelDestinations.FOLDERS)
+    verify(navHostController).navigate(eq(Route.LIST_FOLDERS), any<NavOptionsBuilder.() -> Unit>())
+
     navigationActions.navigateTo(Screen.CAMERA)
     verify(navHostController).navigate(Screen.CAMERA)
 
-    navigationActions.navigateTo(Screen.OTHERS)
-    verify(navHostController).navigate(Screen.OTHERS)
+    navigationActions.navigateTo(Screen.PROFILE)
+    verify(navHostController).navigate(Screen.PROFILE)
+
+    navigationActions.navigateTo(Screen.LIST_FOLDERS)
+    verify(navHostController).navigate(Screen.LIST_FOLDERS)
+
+    navigationActions.navigateTo(Screen.CREATE_FOLDER)
+    verify(navHostController).navigate(Screen.CREATE_FOLDER)
+
+    navigationActions.navigateTo(Screen.CREATE_FILE)
+    verify(navHostController).navigate(Screen.CREATE_FILE)
+
+    navigationActions.navigateTo(Screen.CALCULATOR)
+    verify(navHostController).navigate(Screen.CALCULATOR)
+
+    navigationActions.navigateTo(Screen.POMODORO)
+    verify(navHostController).navigate(Screen.POMODORO)
+
+    navigationActions.navigateTo(Screen.PDF_CONVERTER)
+    verify(navHostController).navigate(Screen.PDF_CONVERTER)
 
     navigationActions.navigateTo(Screen.VIDEOS)
     verify(navHostController).navigate(Screen.VIDEOS)
