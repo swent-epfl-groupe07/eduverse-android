349bab3ac44f71faf71e4b596e4f7c4aaccd6ddb
AKallala <ahmed.kallala@hotmail.fr>
2024-11-03 11:57:23 +0100

    Update PdfConverterViewModel.kt

--- app/src/main/java/com/github/se/eduverse/viewmodel/PdfConverterViewModel.kt
+++ app/src/main/java/com/github/se/eduverse/viewmodel/PdfConverterViewModel.kt
@@ -6,23 +6,52 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.pdf.PdfDocument
 import android.net.Uri
+import android.os.Environment
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.se.eduverse.showToast
+import com.github.se.eduverse.ui.converter.PdfConverterOption
 import java.io.File
 import java.io.FileOutputStream
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-open class PdfConverterViewModel : ViewModel() {
+class PdfConverterViewModel : ViewModel() {
 
-  private val newFileName_ = MutableStateFlow<String>("")
-  val newFileName: StateFlow<String> = newFileName_.asStateFlow()
+  val TEXT_PAGE_TOP_PADDING = 40f
+  val TEXT_PAGE_LEFT_PADDING = 20f
+  val A4_PAGE_WIDTH = 595
+  val A4_PAGE_HEIGHT = 842
+  val DEFAULT_DESTINATION_DIRECTORY =
+      Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+
+  /** Sealed class representing the different states of the PDF generation process */
+  sealed class PdfGenerationState {
+    data object Ready : PdfGenerationState()
+
+    data object InProgress : PdfGenerationState()
+
+    data object Aborted : PdfGenerationState()
+
+    data class Success(val pdfFile: File) : PdfGenerationState()
+
+    data object Error : PdfGenerationState()
+  }
+
+  private val _newFileName = MutableStateFlow<String>("")
+  val newFileName: StateFlow<String> = _newFileName.asStateFlow()
+
+  private val _pdfGenerationState = MutableStateFlow<PdfGenerationState>(PdfGenerationState.Ready)
+  val pdfGenerationState: StateFlow<PdfGenerationState> = _pdfGenerationState.asStateFlow()
+
+  private var currentFile: File? = null
+
+  private var pdfGenerationJob: Job? = null
 
   /**
    * Set the new file name for the PDF to be created
@@ -32,7 +61,37 @@ open class PdfConverterViewModel : ViewModel() {
   fun setNewFileName(pdfFileName: String) {
     // Sanitize the file name to prevent malicious file names
     val sanitizedFileName = pdfFileName.replace(Regex("[^a-zA-Z0-9._-]"), "_")
-    newFileName_.value = sanitizedFileName
+    _newFileName.value = sanitizedFileName
+  }
+
+  /**
+   * Handle the generation of a PDF file from the given URI using the specified converter option
+   *
+   * @param uri The URI of the file to convert to PDF
+   * @param context The context of the application
+   * @param converterOption The option to use to convert the file to PDF
+   */
+  fun generatePdf(uri: Uri?, context: Context, converterOption: PdfConverterOption) {
+    _pdfGenerationState.value = PdfGenerationState.InProgress
+    pdfGenerationJob =
+        viewModelScope.launch(Dispatchers.IO) {
+          try {
+            val pdfFile: PdfDocument =
+                when (converterOption) {
+                  PdfConverterOption.IMAGE_TO_PDF -> convertImageToPdf(uri, context)
+                  PdfConverterOption.TEXT_TO_PDF -> convertTextToPdf(uri, context)
+                  PdfConverterOption.DOCUMENT_TO_PDF -> TODO()
+                  PdfConverterOption.SUMMARIZE_FILE -> TODO()
+                  PdfConverterOption.EXTRACT_TEXT -> TODO()
+                  PdfConverterOption.NONE ->
+                      throw Exception("No converter option selected") // Should never happen
+                }
+            _pdfGenerationState.value = PdfGenerationState.Success(savePdf(pdfFile))
+          } catch (e: Exception) {
+            Log.e("generatePdf", "Failed to generate pdf", e)
+            _pdfGenerationState.value = PdfGenerationState.Error
+          }
+        }
   }
 
   /**
@@ -40,77 +99,38 @@ open class PdfConverterViewModel : ViewModel() {
    *
    * @param imageUri The URI of the image to convert
    * @param context The context of the application
+   * @return The PDF document created from the image
+   * @throws Exception If an error occurs during the conversion process
    */
-  fun convertImageToPdf(imageUri: Uri?, context: Context) {
-    viewModelScope.launch(Dispatchers.IO) {
-      try {
-        // Read the image bitmap from the URI and display a toast if the inputStream fails to open
-        val imageBitmap =
-            context.contentResolver.openInputStream(imageUri!!)?.use { inputStream ->
-              BitmapFactory.decodeStream(inputStream)
+  private fun convertImageToPdf(imageUri: Uri?, context: Context): PdfDocument {
+    try {
+      // Read the image bitmap from the URI
+      val imageBitmap =
+          context.contentResolver.openInputStream(imageUri!!).use { inputStream ->
+            if (inputStream == null) {
+              throw Exception("Failed to open image file")
             }
-                ?: run {
-                  withContext(Dispatchers.Main) {
-                    Log.e("convertImageToPdf", "Failed to decode image")
-                    context.showToast("Failed to open image")
-                  }
-                  return@launch
-                }
+            BitmapFactory.decodeStream(inputStream)
+          }
 
-        // Create a new PDF document and add the image to it
-        val pdfDocument = PdfDocument()
-        val pageInfo =
-            PdfDocument.PageInfo.Builder(imageBitmap.width, imageBitmap.height, 1)
-                .create() // The pdf page will have the same size as the input image
-        val page = pdfDocument.startPage(pageInfo)
-        val canvas = page.canvas
-        canvas.drawBitmap(
-            imageBitmap,
-            0f,
-            0f,
-            null) // Draw the image on the canvas starting from the top-left corner
-        pdfDocument.finishPage(page)
-
-        // Create the PDF file, write the document to it, and generate a toast if the file creation
-        // is successful
-        val file =
-            createUniqueFile(
-                context.filesDir,
-                "${newFileName_.value}.pdf") // For now, save the PDF in the app's files directory,
-        // later will add option to give user choice of
-        // location
-        FileOutputStream(file).use { outputStream -> pdfDocument.writeTo(outputStream) }
-        pdfDocument.close()
-        withContext(Dispatchers.Main) { context.showToast("PDF created successfully") }
-      } catch (e: Exception) {
-        // If an exception occurs during the conversion process, log the error and display a toast
-        Log.e("convertImageToPdf", "Image conversion failed", e)
-        withContext(Dispatchers.Main) { context.showToast("Failed to create PDF") }
-      }
-    }
-  }
-
-  /**
-   * Convert the document corresponding to the given URI to a PDF (for now only handles .docx and
-   * .txt files, will add later other file types)
-   *
-   * @param fileUri The URI of the document to convert
-   * @param context The context of the application
-   */
-  fun convertDocumentToPdf(fileUri: Uri?, context: Context) {
-    val mimeType = context.contentResolver.getType(fileUri!!)
-    when (mimeType) {
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" -> {
-        convertWordToPdf(fileUri, context)
-      }
-      "text/plain" -> {
-        convertTextToPdf(fileUri, context)
-      }
-      // Not needed because the file picker only allows the selection of text and word files, but
-      // kept to be safe
-      else -> {
-        context.showToast("Unsupported file type")
-      }
+      // Create a new PDF document and add the image to it
+      val pdfDocument = PdfDocument()
+      val pageInfo =
+          PdfDocument.PageInfo.Builder(imageBitmap.width, imageBitmap.height, 1)
+              .create() // The pdf page will have the same size as the input image
+      val page = pdfDocument.startPage(pageInfo)
+      val canvas = page.canvas
+      canvas.drawBitmap(
+          imageBitmap,
+          0f,
+          0f,
+          null) // Draw the image on the canvas starting from the top-left corner
+      pdfDocument.finishPage(page)
+      return pdfDocument
+    } catch (e: Exception) {
+      // If an exception occurs during the conversion process, log the error and display a toast
+      Log.e("convertImageToPdf", "Image conversion failed", e)
+      throw e
     }
   }
 
@@ -119,112 +139,152 @@ open class PdfConverterViewModel : ViewModel() {
    *
    * @param fileUri The URI of the text file to convert
    * @param context The context of the application
+   * @return The PDF document created from the text file
+   * @throws Exception If an error occurs during the conversion process
    */
-  fun convertTextToPdf(fileUri: Uri, context: Context) {
-    viewModelScope.launch(Dispatchers.IO) {
-      try {
-        // Open the text file input stream and create a buffered reader on it, display a toast if
-        // the file fails to open
-        val inputStream = context.contentResolver.openInputStream(fileUri)
-        val bufferedReader =
-            inputStream?.bufferedReader()
-                ?: run {
-                  withContext(Dispatchers.Main) {
-                    Log.e("convertTextToPdf", "Failed to open text file")
-                    context.showToast("Failed to open text file")
-                  }
-                  return@launch
-                }
+  private fun convertTextToPdf(fileUri: Uri?, context: Context): PdfDocument {
+    try {
+      // Open the text file input stream and create a buffered reader on it
+      val inputStream =
+          context.contentResolver.openInputStream(fileUri!!)
+              ?: run { throw Exception("Failed to open text file") }
+      val bufferedReader = inputStream.bufferedReader()
 
-        // Create a new PDF document
-        val pdfDocument = PdfDocument()
-        val pageInfo = PdfDocument.PageInfo.Builder(595, 842, 1).create() // A4 page size
-        var page = pdfDocument.startPage(pageInfo)
-        var canvas = page.canvas
-        val paint =
-            Paint()
-                .apply { // Set the paint properties for the text to be written on the pdf document
-                  isAntiAlias = true
-                  textSize = 12f
-                  color = Color.BLACK
-                }
-        var yPosition = 40f // Start writing the text from 40px from the top of the page
+      // Create a new PDF document
+      val pdfDocument = PdfDocument()
+      val pageInfo =
+          PdfDocument.PageInfo.Builder(A4_PAGE_WIDTH, A4_PAGE_HEIGHT, 1).create() // A4 page size
+      var page = pdfDocument.startPage(pageInfo)
+      var canvas = page.canvas
+      val paint =
+          Paint().apply { // Set the paint properties for the text to be written on the pdf document
+            isAntiAlias = true
+            textSize = 12f
+            color = Color.BLACK
+          }
+      var yPosition =
+          TEXT_PAGE_TOP_PADDING // Start writing the text from 40px from the top of the page
 
-        // Read the text from the file in buffered way to avoid memory issues
-        bufferedReader.use { reader ->
-          var line: String?
-          while (reader.readLine().also { line = it } != null) {
-            // Split the line into words to avoid overflowing the page width in case of long lines
-            val words = line!!.split(" ")
+      // Read the text from the file in buffered way to avoid memory issues
+      bufferedReader.use { reader ->
+        var line: String?
+        while (reader.readLine().also { line = it } != null) {
+          // Split the line into words to avoid overflowing the page width in case of long lines
+          val words = line!!.split(" ")
 
-            // Draw the read line word by word into the pdf document
-            var currentLine = ""
-            for (word in words) {
-              // Check if the line with the next word fits in the pdf page width
-              val tempLine =
-                  if (currentLine.isEmpty()) word
-                  else "$currentLine $word" // Add the next word to the current line after a space,
-              // for the first line of the doc no space needed before
-              // the word
-              val textWidth = paint.measureText(tempLine)
-              if (textWidth <= pageInfo.pageWidth - 40) { // 20px padding on each side of the page
-                currentLine = tempLine
-              } else {
-                // Draw the current line and start a new line with the current word
-                canvas.drawText(currentLine, 20f, yPosition, paint)
-                yPosition += paint.descent() - paint.ascent()
-                currentLine = word
-              }
-
-              // If the current page is full, create a new page and add it to the pdf document
-              if (yPosition >
-                  pageInfo.pageHeight - 80) { // 40px padding on top and bottom of the page
-                pdfDocument.finishPage(page)
-                yPosition = 40f
-                val newPageInfo =
-                    PdfDocument.PageInfo.Builder(595, 842, pdfDocument.pages.size + 1).create()
-                page = pdfDocument.startPage(newPageInfo)
-                canvas = page.canvas
-              }
-            }
-
-            // Draw the last word of the read line if it didn't fit in the page's previous line
-            if (currentLine.isNotEmpty()) {
-              canvas.drawText(currentLine, 10f, yPosition, paint)
+          // Draw the read line word by word into the pdf document
+          var currentLine = ""
+          for (word in words) {
+            // Check if the line with the next word fits in the pdf page width
+            val tempLine =
+                if (currentLine.isEmpty()) word
+                else "$currentLine $word" // Add the next word to the current line after a space,
+            // for the first line of the doc no space needed before
+            // the word
+            val textWidth = paint.measureText(tempLine)
+            if (textWidth <= pageInfo.pageWidth - 40) { // 20px padding on each side of the page
+              currentLine = tempLine
+            } else {
+              // Draw the current line and start a new line with the current word
+              canvas.drawText(currentLine, TEXT_PAGE_LEFT_PADDING, yPosition, paint)
               yPosition += paint.descent() - paint.ascent()
+              currentLine = word
             }
 
-            // If the last word of the read line was written on the last line of the page, create a
-            // new page
-            if (yPosition > pageInfo.pageHeight - 40) {
+            // If the current page is full, create a new page and add it to the pdf document
+            if (yPosition >
+                pageInfo.pageHeight - 80) { // 40px padding on top and bottom of the page
               pdfDocument.finishPage(page)
-              yPosition = 40f
+              yPosition = TEXT_PAGE_TOP_PADDING
               val newPageInfo =
-                  PdfDocument.PageInfo.Builder(595, 842, pdfDocument.pages.size + 1).create()
+                  PdfDocument.PageInfo.Builder(
+                          A4_PAGE_WIDTH, A4_PAGE_HEIGHT, pdfDocument.pages.size + 1)
+                      .create()
               page = pdfDocument.startPage(newPageInfo)
               canvas = page.canvas
             }
           }
-        }
 
-        // Finish the last page and write the pdf document to the destination file, display a toast
-        // if document creation is successful
-        pdfDocument.finishPage(page)
-        val file = createUniqueFile(context.filesDir, "${newFileName_.value}.pdf")
-        FileOutputStream(file).use { outputStream -> pdfDocument.writeTo(outputStream) }
-        pdfDocument.close()
-        withContext(Dispatchers.Main) { context.showToast("PDF created successfully") }
-      } catch (e: Exception) {
-        // If an exception occurs during the conversion process, log the error and display a toast
-        Log.e("convertTextToPdf", "Text conversion failed", e)
-        withContext(Dispatchers.Main) { context.showToast("Failed to create PDF") }
+          // Draw the last word of the read line if it didn't fit in the page's previous line
+          if (currentLine.isNotEmpty()) {
+            canvas.drawText(currentLine, TEXT_PAGE_LEFT_PADDING, yPosition, paint)
+            yPosition += paint.descent() - paint.ascent()
+          }
+
+          // If the last word of the read line was written on the last line of the page, create a
+          // new page
+          if (yPosition > pageInfo.pageHeight - 40) {
+            pdfDocument.finishPage(page)
+            yPosition = TEXT_PAGE_TOP_PADDING
+            val newPageInfo =
+                PdfDocument.PageInfo.Builder(
+                        A4_PAGE_WIDTH, A4_PAGE_HEIGHT, pdfDocument.pages.size + 1)
+                    .create()
+            page = pdfDocument.startPage(newPageInfo)
+            canvas = page.canvas
+          }
+        }
       }
+
+      // Finish the last page and write the pdf document to the destination file, display a toast
+      // if document creation is successful
+      pdfDocument.finishPage(page)
+
+      return pdfDocument
+    } catch (e: Exception) {
+      // If an exception occurs during the conversion process, log the error and throw the exception
+      Log.e("convertTextToPdf", "Text conversion failed", e)
+      throw e
     }
   }
 
   // Not implemented yet
-  fun convertWordToPdf(fileUri: Uri, context: Context) {
+  private fun convertWordToPdf(fileUri: Uri, context: Context) {
     viewModelScope.launch { context.showToast("Not implemented yet") }
+  }
+
+  /**
+   * Save the given PDF document to the documents directory of the device
+   *
+   * @param pdfDocument The PDF document to save
+   * @return The file where the PDF document was saved
+   * @throws Exception If an error occurs during the saving process
+   */
+  private fun savePdf(pdfDocument: PdfDocument): File {
+    try {
+      val file = createUniqueFile(DEFAULT_DESTINATION_DIRECTORY, "${newFileName.value}.pdf")
+      FileOutputStream(file).use { outputStream -> pdfDocument.writeTo(outputStream) }
+      pdfDocument.close()
+      currentFile = file
+      return file
+    } catch (e: Exception) {
+      pdfDocument
+          .close() // Close the pdf document in case of an exception when writing to the output file
+      Log.e("savePdf", "Failed to save pdf to documents directory", e)
+      throw e
+    }
+  }
+
+  /** Set the PDF generation state to ready and reset the current file */
+  fun setPdfGenerationStateToReady() {
+    _pdfGenerationState.value = PdfGenerationState.Ready
+    currentFile = null
+  }
+
+  /** Abort the PDF generation process if it's in progress */
+  fun abortPdfGeneration() {
+    if (pdfGenerationJob != null) {
+      if (pdfGenerationJob!!.isActive) {
+        pdfGenerationJob!!.cancel()
+        // In case the job is cancelled before the job is completed but after the file has been
+        // saved to the device set the generation state to success
+        if (currentFile != null) {
+          _pdfGenerationState.value = PdfGenerationState.Success(currentFile!!)
+        } else {
+          _pdfGenerationState.value = PdfGenerationState.Aborted
+        }
+      }
+    }
   }
 
   /**
