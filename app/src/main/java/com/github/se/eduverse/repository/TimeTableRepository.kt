9a1bd23b890f96e2707fe1d6b3d0e286cd3e7b56
Mathieu COLLET <mathieu.collet@epfl.ch>
2024-11-06 17:32:01 +0100

    Implement the time table repository

--- app/src/main/java/com/github/se/eduverse/repository/TimeTableRepository.kt
+++ app/src/main/java/com/github/se/eduverse/repository/TimeTableRepository.kt
@@ -1,4 +1,14 @@
 package com.github.se.eduverse.repository
 
+import com.github.se.eduverse.model.Scheduled
+import java.util.Calendar
+
 interface TimeTableRepository {
+    fun getScheduled(firstDay: Calendar, ownerId: String, onSuccess: (List<Scheduled>) -> Unit, onFailure: (Exception) -> Unit)
+
+    fun addScheduled(scheduled: Scheduled, onSuccess: () -> Unit, onFailure: (Exception) -> Unit)
+
+    fun updateScheduled(scheduled: Scheduled, onSuccess: () -> Unit, onFailure: (Exception) -> Unit)
+
+    fun deleteScheduled(scheduled: Scheduled, onSuccess: () -> Unit, onFailure: (Exception) -> Unit)
 }
