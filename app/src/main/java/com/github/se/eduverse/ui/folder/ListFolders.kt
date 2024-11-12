bbf3584c4550e2ef8c05e958f0215b753413445c
Mathieu COLLET <mathieu.collet@epfl.ch>
2024-11-12 21:55:01 +0100

    Implement the archive screen

--- app/src/main/java/com/github/se/eduverse/ui/folder/ListFolders.kt
+++ app/src/main/java/com/github/se/eduverse/ui/folder/ListFolders.kt
@@ -1,6 +1,5 @@
 package com.github.se.eduverse.ui.folder
 
-//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -18,12 +17,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -31,7 +27,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.github.se.eduverse.model.Folder
 import com.github.se.eduverse.ui.navigation.BottomNavigationMenu
@@ -44,43 +39,38 @@ import com.github.se.eduverse.viewmodel.FolderViewModel
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListFoldersScreen(navigationActions: NavigationActions, folderViewModel: FolderViewModel) {
-  val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
   folderViewModel.getUserFolders()
   val folders: List<Folder> by folderViewModel.folders.collectAsState()
 
   Scaffold(
       topBar = {
-          CenterAlignedTopAppBar(
-              modifier = Modifier.testTag("topAppBar"),
-              title = {
-                  Row(
-                      modifier = Modifier.fillMaxWidth(),
-                      horizontalArrangement = Arrangement.SpaceBetween,
-                      verticalAlignment = Alignment.CenterVertically) {
-                      Text(
-                          text = "My Courses", modifier = Modifier.testTag("topBarText"))
-                      IconButton(onClick = {}, modifier = Modifier.testTag("archive")) {
-                          Icon(imageVector = Icons.Default.Archive, contentDescription = "Archive")
-                      }
+        CenterAlignedTopAppBar(
+            modifier = Modifier.testTag("topAppBar"),
+            title = {
+              Row(
+                  modifier = Modifier.fillMaxWidth(),
+                  horizontalArrangement = Arrangement.SpaceBetween,
+                  verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = "My Courses", modifier = Modifier.testTag("topBarText"))
+                    IconButton(onClick = {}, modifier = Modifier.testTag("archive")) {
+                      Icon(imageVector = Icons.Default.Archive, contentDescription = "Archive")
+                    }
                   }
-              },
-              navigationIcon = {
-                  IconButton(
-                      onClick = { navigationActions.goBack() }, modifier = Modifier.testTag("goBack")) {
-                      Icon(
-                          imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                          contentDescription = "Back Arrow")
+            },
+            navigationIcon = {
+              IconButton(
+                  onClick = { navigationActions.goBack() }, modifier = Modifier.testTag("goBack")) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back Arrow")
                   }
-              },
-              colors =
-              TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Color.Transparent))
+            },
+            colors =
+                TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Color.Transparent))
       },
       bottomBar = {
         BottomNavigationMenu(
-            { navigationActions.navigateTo(it) },
-            LIST_TOP_LEVEL_DESTINATION,
-            Route
-                .LIST_FOLDERS) // No item is selected, as it is not one of the screens on the bottom
+            { navigationActions.navigateTo(it) }, LIST_TOP_LEVEL_DESTINATION, Route.LIST_FOLDERS)
         // bar
       },
       floatingActionButton = {
