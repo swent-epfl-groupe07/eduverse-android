febc3b7cca25c6cf21ff83a23cfe9aa05244a87a
AKallala <ahmed.kallala@hotmail.fr>
2024-11-14 01:32:40 +0100

    Update TodoListScreen.kt: add a dialog to rename a todo and update the todo item to make it reusable

--- app/src/main/java/com/github/se/eduverse/ui/todo/TodoListScreen.kt
+++ app/src/main/java/com/github/se/eduverse/ui/todo/TodoListScreen.kt
@@ -10,10 +10,14 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.rounded.Done
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,7 +27,6 @@ import com.github.se.eduverse.model.TodoStatus
 import com.github.se.eduverse.ui.navigation.NavigationActions
 import com.github.se.eduverse.ui.navigation.TopNavigationBar
 import com.github.se.eduverse.viewmodel.TodoListViewModel
-import kotlinx.serialization.json.JsonNull.content
 
 /** Composable that represents the user's todo list screen */
 @Composable
@@ -58,16 +61,37 @@ fun TodoListScreen(navigationActions: NavigationActions, todoListViewModel: Todo
                   Text("Completed Tasks", modifier = Modifier.padding(8.dp), fontSize = 20.sp)
                 }
           }
-          LazyColumn(modifier = Modifier.padding(16.dp)) {
+          LazyColumn(modifier = Modifier.fillMaxSize().padding(top = 8.dp)) {
             val todosToShow = if (showCompleted) doneTodos.value else actualTodos.value
             items(todosToShow.size) { i ->
               val todo = todosToShow[i]
+              var showRenameDialog by remember { mutableStateOf(false) }
               TodoItem(
                   todo,
-                  { todoListViewModel.setTodoActual(it) },
-                  { todoListViewModel.setTodoDone(it) },
-                  { todoListViewModel.deleteTodo(it.uid) },
-                  { currTodo, newName -> todoListViewModel.renameTodo(currTodo, newName) })
+                  { todoListViewModel.setTodoActual(todo) },
+                  { todoListViewModel.setTodoDone(todo) }) {
+                    Box {
+                      var expanded by remember { mutableStateOf(false) }
+                      IconButton(
+                          onClick = { expanded = !expanded },
+                          modifier =
+                              Modifier.testTag("todoOptionsButton_${todo.uid}").padding(8.dp)) {
+                            Icon(Icons.Default.MoreVert, contentDescription = "Todo Options")
+                          }
+                      TodoOptionsMenu(
+                          todo.uid,
+                          expanded,
+                          { expanded = false },
+                          { todoListViewModel.deleteTodo(todo.uid) },
+                          { showRenameDialog = true })
+                    }
+                  }
+              if (showRenameDialog) {
+                RenameTodoDialog(
+                    todo.name,
+                    { newName -> todoListViewModel.renameTodo(todo, newName) },
+                    { showRenameDialog = false })
+              }
             }
           }
         }
@@ -80,30 +104,32 @@ fun TodoListScreen(navigationActions: NavigationActions, todoListViewModel: Todo
  * @param todo the todo to display
  * @param onUndo code executed when the done button is clicked while the todo is completed
  * @param onDone code executed when the done button is clicked while the todo is not completed
+ * @param rightMostButton the button to display on the right side of the todo item, it depends on
+ *   the screen the todo is displayed on
  */
 @Composable
 fun TodoItem(
     todo: Todo,
-    onUndo: (Todo) -> Unit,
-    onDone: (Todo) -> Unit,
-    onDelete: (Todo) -> Unit,
-    onRename: (Todo, String) -> Unit
+    onUndo: () -> Unit,
+    onDone: () -> Unit,
+    rightMostButton: @Composable () -> Unit
 ) {
   val completed = todo.status == TodoStatus.DONE
+
   Card(
       modifier =
-          Modifier.testTag("todoItem_${todo.uid}")
-              .wrapContentSize()
+          Modifier.height(48.dp)
+              .padding(start = 24.dp, end = 24.dp, bottom = 8.dp)
               .fillMaxWidth()
-              .padding(vertical = 4.dp),
+              .testTag("todoItem_${todo.uid}"),
       shape = RoundedCornerShape(16.dp)) {
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.wrapContentSize()) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
           IconButton(
               onClick = {
                 if (completed) {
-                  onUndo(todo)
+                  onUndo()
                 } else {
-                  onDone(todo)
+                  onDone()
                 }
               },
               modifier = Modifier.testTag("todoDoneButton_${todo.uid}").padding(8.dp).size(24.dp),
@@ -127,15 +153,7 @@ fun TodoItem(
               textDecoration = if (completed) TextDecoration.LineThrough else TextDecoration.None,
               color = if (completed) Color.Gray else Color.Unspecified)
 
-          Box {
-            var expanded by remember { mutableStateOf(false) }
-            IconButton(
-                onClick = { expanded = !expanded },
-                modifier = Modifier.testTag("todoOptionsButton_${todo.uid}").padding(8.dp)) {
-                  Icon(Icons.Default.MoreVert, contentDescription = "Todo Options")
-                }
-            TodoOptionsMenu(todo.uid, expanded, { expanded = false }, { onDelete(todo) }, {})
-          }
+          rightMostButton()
         }
       }
 }
@@ -170,6 +188,7 @@ fun AddTodoEntry(onAdd: (String) -> Unit) {
               modifier =
                   Modifier.weight(1f).padding(start = 8.dp, end = 8.dp).testTag("addTodoTextField"),
               textStyle = LocalTextStyle.current.copy(fontSize = 18.sp, color = Color.Black),
+              singleLine = true,
               decorationBox = { innerTextField ->
                 if (name.isEmpty()) {
                   Text(text = "Add a new Task", color = Color.Gray)
@@ -217,4 +236,50 @@ fun TodoOptionsMenu(
             },
             modifier = Modifier.testTag("renameTodoButton_${todoId}"))
       }
+}
+
+/**
+ * Composable that represents a dialog to rename a todo
+ *
+ * @param todoName the current name of the todo
+ * @param onRename code executed when the rename button is clicked
+ * @param onDismiss code executed when the dialog is dismissed
+ */
+@Composable
+fun RenameTodoDialog(todoName: String, onRename: (String) -> Unit, onDismiss: () -> Unit) {
+  var newName by remember { mutableStateOf(TextFieldValue(todoName, TextRange(todoName.length))) }
+  AlertDialog(
+      onDismissRequest = { onDismiss() },
+      title = { Text("Rename Todo") },
+      text = {
+        OutlinedTextField(
+            value = newName,
+            onValueChange = { newName = it },
+            singleLine = true,
+            modifier = Modifier.padding(8.dp).fillMaxWidth().testTag("renameTodoTextField"),
+            shape = RoundedCornerShape(32.dp),
+        )
+      },
+      confirmButton = {
+        TextButton(
+            onClick = {
+              onRename(newName.text)
+              onDismiss()
+            },
+            enabled = newName.text.isNotEmpty(),
+            colors =
+                ButtonColors(
+                    Color.Transparent, Color(0xFF217384), Color.Transparent, Color.LightGray)) {
+              Text("Rename")
+            }
+      },
+      dismissButton = {
+        TextButton(
+            onClick = { onDismiss() },
+            colors =
+                ButtonColors(
+                    Color.Transparent, Color(0xFF217384), Color.Transparent, Color.LightGray)) {
+              Text("Cancel")
+            }
+      })
 }
