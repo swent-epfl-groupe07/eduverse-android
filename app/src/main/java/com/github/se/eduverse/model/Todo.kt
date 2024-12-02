dc867dd01b7a6444fcc5ba883f62d1591fc7d7bc
AKallala <ahmed.kallala@hotmail.fr>
2024-12-02 11:12:31 +0100

    Update Todo.kt: add a creationTime attribute to order the todos on it and have a consistent display of the list

--- app/src/main/java/com/github/se/eduverse/model/Todo.kt
+++ app/src/main/java/com/github/se/eduverse/model/Todo.kt
@@ -1,11 +1,15 @@
 package com.github.se.eduverse.model
 
+import java.util.Calendar
+import java.util.Date
+
 data class Todo(
     val uid: String,
     val name: String,
     val timeSpent: Long = 0, // in minutes
     val status: TodoStatus,
-    val ownerId: String
+    val ownerId: String,
+    val creationTime: Date = Date() // used to sort the todos
 )
 
 enum class TodoStatus {
