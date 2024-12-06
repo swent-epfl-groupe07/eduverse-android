e0f6d6cf1f9aca78631ceb37325ff1313ead1b23
AKallala <ahmed.kallala@hotmail.fr>
2024-12-06 11:17:28 +0100

    Update topNavigationBarIsCorrectlyDisplayed test after modification of the screen title

--- app/src/androidTest/java/com/github/se/eduverse/ui/pdfGenerator/PdfGeneratorScreenTest.kt
+++ app/src/androidTest/java/com/github/se/eduverse/ui/pdfGenerator/PdfGeneratorScreenTest.kt
@@ -74,7 +74,7 @@ class PdfGeneratorScreenTest {
     composeTestRule.setContent { PdfGeneratorScreen(mockNavigationActions, pdfGeneratorViewModel) }
     composeTestRule.onNodeWithTag("topNavigationBar").assertIsDisplayed()
     composeTestRule.onNodeWithTag("screenTitle").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("screenTitle").assertTextEquals("PDF Converter")
+    composeTestRule.onNodeWithTag("screenTitle").assertTextEquals("PDF Generator")
     composeTestRule.onNodeWithTag("goBackButton").assertIsDisplayed()
     composeTestRule.onNodeWithTag("goBackButton").performClick()
     verify(mockNavigationActions).goBack()
