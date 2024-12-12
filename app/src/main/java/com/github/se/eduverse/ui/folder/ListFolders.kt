cfe75fa6b156fed3aa575078ff1c7243fbf22add
Mathieu COLLET <mathieu.collet@epfl.ch>
2024-12-12 11:09:32 +0100

    Add possibility to select and delete folders from ListFolders

--- app/src/main/java/com/github/se/eduverse/ui/folder/ListFolders.kt
+++ app/src/main/java/com/github/se/eduverse/ui/folder/ListFolders.kt
@@ -1,17 +1,26 @@
 package com.github.se.eduverse.ui.folder
 
 import android.content.Context
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.FloatingActionButtonDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Archive
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.CheckBox
+import androidx.compose.material.icons.filled.CheckBoxOutlineBlank
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +30,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
@@ -35,6 +47,7 @@ import com.github.se.eduverse.ui.navigation.Screen
 import com.github.se.eduverse.ui.navigation.TopNavigationBar
 import com.github.se.eduverse.viewmodel.FolderViewModel
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ListFoldersScreen(
     navigationActions: NavigationActions,
@@ -42,12 +55,27 @@ fun ListFoldersScreen(
     context: Context = LocalContext.current
 ) {
   val folders: List<Folder> by folderViewModel.folders.collectAsState()
+    var isSelectMode by remember { mutableStateOf(false) }
+    var selected = emptyList<Int>()
 
   LaunchedEffect(Unit) { folderViewModel.getUserFolders() }
 
   Scaffold(
       topBar = {
         TopNavigationBar("My Courses", navigationActions) {
+            if (isSelectMode) {
+                IconButton(
+                    onClick = {
+                        isSelectMode = false
+                        selected.forEach {
+                            folderViewModel.deleteFolder(folders[it])
+                        }
+                        selected = emptyList()
+                    },
+                    modifier = Modifier.testTag("delete")) {
+                    Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+                }
+            }
           IconButton(
               onClick = { navigationActions.navigateTo(Route.ARCHIVE) },
               modifier = Modifier.testTag("archive")) {
@@ -75,26 +103,73 @@ fun ListFoldersScreen(
               Icon(Icons.Default.Add, contentDescription = "Create Folder")
             }
       }) { padding ->
-        LazyColumn(modifier = Modifier.padding(padding).fillMaxSize()) {
-          item {
-            folders.forEach {
-              Card(
-                  modifier =
-                      Modifier.padding(8.dp)
-                          .fillMaxWidth()
-                          .clickable {
-                            folderViewModel.selectFolder(it)
-                            navigationActions.navigateTo(Screen.FOLDER)
-                          }
-                          .testTag("folderCard${it.id}"),
-                  elevation = 4.dp) {
-                    Text(
-                        text = it.name,
-                        modifier = Modifier.padding(16.dp),
-                        style = androidx.compose.material.MaterialTheme.typography.h6)
-                  }
+        LazyColumn(modifier = Modifier
+            .padding(padding)
+            .fillMaxSize()) {
+            if (isSelectMode) {
+                item {
+                    Row(
+                        modifier = Modifier.clickable {
+                            isSelectMode = false
+                            selected = emptyList()
+                        }
+                    ) {
+                        Icon(Icons.Default.Cancel, contentDescription = "Cancel Selection")
+                        Text("Cancel selection")
+                    }
+                }
             }
-          }
+            items(folders.size) {
+                Card(
+                    modifier =
+                    Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth()
+                        .combinedClickable(
+                            onLongClick = {
+                                if (!isSelectMode) {
+                                    isSelectMode = true
+                                    if (!selected.contains(it)) {
+                                        selected = selected + it
+                                    }
+                                }
+                            },
+                            onClick = {
+                                folderViewModel.selectFolder(folders[it])
+                                navigationActions.navigateTo(Screen.FOLDER)
+                            }
+                        )
+                        .testTag("folderCard${folders[it].id}"),
+                    elevation = 4.dp) {
+                    Row(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = folders[it].name,
+                            style = androidx.compose.material.MaterialTheme.typography.h6
+                        )
+                        if (isSelectMode) {
+                            if (selected.contains(it)) {
+                                IconButton(
+                                    onClick = { selected = selected - it }
+                                ) {
+                                    Icon(Icons.Default.CheckBox, contentDescription = "Checked")
+                                }
+
+                            } else {
+                                IconButton(
+                                    onClick = { selected = selected + it }
+                                ) {
+                                    Icon(Icons.Default.CheckBoxOutlineBlank, contentDescription = "Not Checked")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
       }
 }
